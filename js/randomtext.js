ea63371feb43348fac3402ea73d25d335dec53d4
DestroyerXL44 <28364459+DXL44@users.noreply.github.com>
2023-02-24 13:57:27 -0500

    ability to reshuffle text on click +  more texts

--- js/randomtext.js
+++ js/randomtext.js
@@ -3,19 +3,33 @@ function randomtext(options)
     return options[Math.floor(Math.random()*options.length)];
 }
 /*THE FRONT PAGE */  
-var options = [
-"Actual website coming soon.", 
-"Actual website not coming soon.", 
-"NOW with even MORE!",
-"reads the entire tpmc wiki page in vc",
-"game ing",
-"est. 2018",
-"DXL44 REAL??????????????",
-"subscribe",
-"dxl juice",
-"Y'ALL READY TO GET FUNKY!?",
-"certified niche internet microcelebrity",
-"I guess this really is the DXL44 Website (2023)",
-"Now featuring a British rail class 44 “peak”" // Galactica
-]
-document.getElementById("randomGreet").innerHTML = (randomtext(options));
+
+function fpRandomText(){
+    var options = [
+        "Actual website coming soon.", 
+        "Actual website not coming soon.", 
+        "NOW with even MORE!",
+        "reads the entire tpmc wiki page in vc",
+        "game ing",
+        "est. 2018",
+        "DXL44 REAL??????????????",
+        "subscribe",
+        "dxl juice",
+        "Y'ALL READY TO GET FUNKY!?",
+        "certified niche internet microcelebrity",
+        "I guess this really is the DXL44 Website (2023)",
+        "Now featuring a British rail class 44 peak", // Galactica
+        "now with 200% more fruit",
+        "Shoutouts to Sevis",
+        "the typical floridian",
+        "The REAL Totalpro64 Fanclub",
+        "dxl44.github.io",
+        "an excuse for me to exercise my webdev muscles",
+        "girlbossing",
+        "totalpro64 was here", //krypt
+    ]
+        document.getElementById("randomGreet").innerHTML = (randomtext(options));
+        
+}
+
+fpRandomText()
